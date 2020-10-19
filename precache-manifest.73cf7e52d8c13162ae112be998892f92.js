85e9a44669e601b5d4753ff41928e0a92d4ea54c
Chondan <chondansusuwan@outlook.com>
2020-10-19 14:19:29 +0700

    uploaded build folder for publishing on github page

---
+++ precache-manifest.73cf7e52d8c13162ae112be998892f92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc8d054b2d3146ea63f012eca5afb79",
+    "url": "/Fully-Responsive-Website/index.html"
+  },
+  {
+    "revision": "ea5855687da3b3856711",
+    "url": "/Fully-Responsive-Website/static/css/main.08faceaf.chunk.css"
+  },
+  {
+    "revision": "c54105d04daa92211558",
+    "url": "/Fully-Responsive-Website/static/js/2.819b176d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Fully-Responsive-Website/static/js/2.819b176d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea5855687da3b3856711",
+    "url": "/Fully-Responsive-Website/static/js/main.ef77d107.chunk.js"
+  },
+  {
+    "revision": "4811c78364dd1f35484c",
+    "url": "/Fully-Responsive-Website/static/js/runtime-main.0947d323.js"
+  }
+]);
